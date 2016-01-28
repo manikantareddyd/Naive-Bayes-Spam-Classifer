57f2ad1d7093c6b20dc23b89afd4c05cc0db3d43
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-01-28 17:16:03 +0530

    NaiveBayesClassifier now implemented

---
+++ run.sh
@@ -0,0 +1,5 @@
+python src/lo.py 1
+python src/lo.py 2
+python src/lo.py 3
+python src/lo.py 4
+python src/lo.py 5
---
+++ run1.sh
@@ -0,0 +1,5 @@
+python src/lo.py 6
+python src/lo.py 7
+python src/lo.py 8
+python src/lo.py 9
+python src/lo.py 10
