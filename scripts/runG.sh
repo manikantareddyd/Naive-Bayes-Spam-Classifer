06fb057834a8e761d0132f031d365caed75b1e82
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-02-03 11:49:14 +0530

    Results Generated

---
+++ scripts/runG.sh
@@ -0,0 +1,32 @@
+python src/Gaussian/lemmatized.py 1 >> Results/Gaussian/lemmatized.txt
+python src/Gaussian/lemmatized.py 2 >> Results/Gaussian/lemmatized.txt
+python src/Gaussian/lemmatized.py 3 >> Results/Gaussian/lemmatized.txt
+python src/Gaussian/lemmatized.py 4 >> Results/Gaussian/lemmatized.txt
+python src/Gaussian/lemmatized.py 5 >> Results/Gaussian/lemmatized.txt
+python src/Gaussian/lemmatized.py 6 >> Results/Gaussian/lemmatized.txt
+python src/Gaussian/lemmatized.py 7 >> Results/Gaussian/lemmatized.txt
+python src/Gaussian/lemmatized.py 8 >> Results/Gaussian/lemmatized.txt
+python src/Gaussian/lemmatized.py 9 >> Results/Gaussian/lemmatized.txt
+python src/Gaussian/lemmatized.py 10 >> Results/Gaussian/lemmatized.txt
+
+python src/Gaussian/stopwords.py 1 >> Results/Gaussian/stopwords.txt
+python src/Gaussian/stopwords.py 2 >> Results/Gaussian/stopwords.txt
+python src/Gaussian/stopwords.py 3 >> Results/Gaussian/stopwords.txt
+python src/Gaussian/stopwords.py 4 >> Results/Gaussian/stopwords.txt
+python src/Gaussian/stopwords.py 5 >> Results/Gaussian/stopwords.txt
+python src/Gaussian/stopwords.py 6 >> Results/Gaussian/stopwords.txt
+python src/Gaussian/stopwords.py 7 >> Results/Gaussian/stopwords.txt
+python src/Gaussian/stopwords.py 8 >> Results/Gaussian/stopwords.txt
+python src/Gaussian/stopwords.py 9 >> Results/Gaussian/stopwords.txt
+python src/Gaussian/stopwords.py 10 >> Results/Gaussian/stopwords.txt
+
+python src/Gaussian/subject-body.py 1 >> Results/Gaussian/subject-body.txt
+python src/Gaussian/subject-body.py 2 >> Results/Gaussian/subject-body.txt
+python src/Gaussian/subject-body.py 3 >> Results/Gaussian/subject-body.txt
+python src/Gaussian/subject-body.py 4 >> Results/Gaussian/subject-body.txt
+python src/Gaussian/subject-body.py 5 >> Results/Gaussian/subject-body.txt
+python src/Gaussian/subject-body.py 6 >> Results/Gaussian/subject-body.txt
+python src/Gaussian/subject-body.py 7 >> Results/Gaussian/subject-body.txt
+python src/Gaussian/subject-body.py 8 >> Results/Gaussian/subject-body.txt
+python src/Gaussian/subject-body.py 9 >> Results/Gaussian/subject-body.txt
+python src/Gaussian/subject-body.py 10 >> Results/Gaussian/subject-body.txt
